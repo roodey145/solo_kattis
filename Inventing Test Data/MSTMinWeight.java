7937c512bad66943c6a4bf2346bca453950649c1
roodey145 <64523090+roodey145@users.noreply.github.com>
2026-07-05 20:26:29 +0200

    Initializing Inventing Test Data class

---
+++ Inventing Test Data/MSTMinWeight.java
@@ -0,0 +1,18 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/***
+ * This class is supposed to find the minimum possible weight
+ * of a complete graph given a minimmum spanning tree. The
+ * class is constraint to give a min weight that ensure the
+ * there would be no possibility for the existance of another 
+ * minimum spinning tree.
+ */
+public class MSTMinWeight {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int testCases = Integer.parseInt(reader.readLine()); // Number of test cases in total
+    }
+}
